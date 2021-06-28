38174aa24e115de1003bd597bd31a56cca3ae665
Mateus de Assis Chacon Lima <mateusdeassislima@gmail.com>
2021-06-28 09:45:39 -0300

    Kotlin: herença, polimorfismo e Interfase: Implementação a calculadora de bonificação

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -31,6 +31,18 @@ fun main() {
     } else {
         println("Falha na autenticação")
     }
+
+    println("-----------------------------")
+    val calculadora = CalculadoraBonificacao()
+
+    calculadora.registra(alex)
+    calculadora.registra(fran)
+    calculadora.registra(gabi)
+    println(" total de bonificação  : ${calculadora.total}")
+
+
+
+
     //https://github.com/zup-academy/nosso-cartao-documentacao/tree/master/orange-talent-5#:~:text=final%20do%20curso-,Kotlin%3A%20heran%C3%A7a%2C%20polimorfismo%20e%20interface,-Clique%20aqui%20para
 }
 
